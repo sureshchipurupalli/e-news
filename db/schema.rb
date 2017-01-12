d1c990c91ee1c7d2466e0932f19ebf9207c012a0
suresh407 <suresh.ch407@gmail.com>
2017-01-12 05:30:50 +0000

    added status to users

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170111103821) do
+ActiveRecord::Schema.define(version: 20170112052718) do
 
   create_table "news", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
     t.text     "title",       limit: 65535
@@ -44,6 +44,7 @@ ActiveRecord::Schema.define(version: 20170111103821) do
     t.string   "last_sign_in_ip"
     t.datetime "created_at",                          null: false
     t.datetime "updated_at",                          null: false
+    t.boolean  "state"
     t.index ["email"], name: "index_users_on_email", unique: true, using: :btree
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
   end
