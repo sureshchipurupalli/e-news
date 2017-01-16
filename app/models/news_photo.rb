b639845d7e09c8c05948584dbf17aa80638d435d
sureshchipurupalli <suresh.ch471@gmail.com>
2017-01-16 06:05:56 -0500

    from siva

--- app/models/news_photo.rb
+++ app/models/news_photo.rb
@@ -1,6 +1,6 @@
 class NewsPhoto < ApplicationRecord
-belongs_to :news
+	belongs_to :news
 
-has_attached_file :image, styles: { medium: "300x300>", thumb: "100x100>" }
-  validates_attachment_content_type :image, content_type: /\Aimage\/.*\Z/
+	has_attached_file :image, styles: { medium: "229x319>", thumb: "100x100>" }
+  	validates_attachment_content_type :image, content_type: /\Aimage\/.*\Z/
 end
