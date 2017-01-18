dd0277a9a6aaf08dc9c3ab9324da247496e4551d
sureshchipurupalli <suresh.ch471@gmail.com>
2017-01-18 06:43:05 -0500

    from adi

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170118104614) do
+ActiveRecord::Schema.define(version: 20170118113252) do
 
   create_table "news", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
     t.text     "title",       limit: 65535
@@ -21,6 +21,7 @@ ActiveRecord::Schema.define(version: 20170118104614) do
     t.datetime "updated_at",                                null: false
     t.boolean  "state",                     default: false
     t.integer  "user_id"
+    t.string   "email"
   end
 
   create_table "news_photos", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
